facb5266b210a66397a599a302254cbf4c53997f
Mika Heares <mheares@idea11.com.au>
2023-07-08 21:45:49 +1000

    added scarecrow proto

---
+++ scenes/ScareCrow.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class ScareCrow : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		base._Ready();
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		animatedSprite2D.Play();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
