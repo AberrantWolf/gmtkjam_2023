fe36ab28bde5977e0033932dd41ada7514c4a67c
Hedd Schotte <subzerofusion@gmail.com>
2023-07-09 05:01:46 +1000

    Updated Crows

--- scenes/Crow.cs
+++ scenes/Crow.cs
@@ -5,109 +5,109 @@ using Godot;
 
 public partial class Crow : Area2D
 {
-    [Export]
-    private string groupName = "crows";
+	[Export]
+	private string groupName = "crows";
 
-    [Export]
-    public int Speed = 100;
+	[Export]
+	public int Speed = 100;
 
-    [Export]
-    public int Separation = 100;
+	[Export]
+	public int Separation = 100;
 
-    private Vector2 lastDirection { get; set; } = Vector2.Right;
+	private Vector2 lastDirection { get; set; } = Vector2.Right;
 
-    private IEnumerable<Crow> allCrows = null;
-    private Vector2 randomMultiplier { get; set; } = Vector2.Right;
-    private int AverageWeighting = 500;
-    private int FramesOfAddedWeight = 0;
-    private int CurrentFramesOfAddedWeight = 0;
-    private int LocalCount = 0;
+	private IEnumerable<Crow> allCrows = null;
+	private Vector2 randomMultiplier { get; set; } = Vector2.Right;
+	private int AverageWeighting = 500;
+	private int FramesOfAddedWeight = 0;
+	private int CurrentFramesOfAddedWeight = 0;
+	private int LocalCount = 0;
 
-    public override void _Input(InputEvent @event)
-    {
-        if (@event is InputEventMouseButton mouseButton && mouseButton.Pressed && mouseButton.ButtonIndex == MouseButton.Left)
-        {
-            CurrentFramesOfAddedWeight = this.FramesOfAddedWeight;
-        }
-    }
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseButton && mouseButton.Pressed && mouseButton.ButtonIndex == MouseButton.Left)
+		{
+			CurrentFramesOfAddedWeight = this.FramesOfAddedWeight;
+		}
+	}
 
-    public override void _ExitTree()
-    {
-        base._ExitTree();
+	public override void _ExitTree()
+	{
+		base._ExitTree();
 
-        CrowHiveMind.Instance.AllCrows.Remove(this);
-    }
+		CrowHiveMind.Instance.AllCrows.Remove(this);
+	}
 
-    public override void _Ready()
-    {
-        base._Ready();
-        var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-        //animatedSprite2D.Play();
-        CrowHiveMind.Instance.AllCrows.Add(this);
-        updateWeightings(CrowHiveMind.Instance.AllCrows);
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		//animatedSprite2D.Play();
+		CrowHiveMind.Instance.AllCrows.Add(this);
+		updateWeightings(CrowHiveMind.Instance.AllCrows);
+	}
 
-    public void updateWeightings(IEnumerable<Crow> Crows)
-    {
-        this.allCrows = Crows;
-        var rng = new RandomNumberGenerator();
+	public void updateWeightings(IEnumerable<Crow> Crows)
+	{
+		this.allCrows = Crows;
+		var rng = new RandomNumberGenerator();
 		this.MonsterBase = rng.RandfRange((float)0.2, (float)1.1);
-        this.AverageWeighting = rng.RandiRange(30, 100);
-        this.FramesOfAddedWeight = rng.RandiRange(50, 250);
-        this.randomMultiplier = new Vector2(rng.Randf(), rng.Randf());
-        this.LocalCount = this.allCrows.Count() / 10;
-        if (this.LocalCount == 0)
-        {
-            this.LocalCount = this.allCrows.Count();
-        }
-    }
+		this.AverageWeighting = rng.RandiRange(30, 100);
+		this.FramesOfAddedWeight = rng.RandiRange(50, 250);
+		this.randomMultiplier = new Vector2(rng.Randf(), rng.Randf());
+		this.LocalCount = this.allCrows.Count() / 10;
+		if (this.LocalCount == 0)
+		{
+			this.LocalCount = this.allCrows.Count();
+		}
+	}
 
 
-    public override void _Process(double delta)
-    {
-        var mouseLocation = CrowHiveMind.Instance.MouseLocation;
+	public override void _Process(double delta)
+	{
+		var mouseLocation = CrowHiveMind.Instance.MouseLocation;
 
-        //Aim towards center
+		//Aim towards center
 
-        var allCrows = CrowHiveMind.Instance.AllCrows;
-        var centerOfMass = CrowHiveMind.Instance.CenterOfMass;
+		var allCrows = CrowHiveMind.Instance.AllCrows;
+		var centerOfMass = CrowHiveMind.Instance.CenterOfMass;
 
-        //avoid flying close
+		//avoid flying close
 
-        var closeCrows = allCrows.Where(crow => crow.Position.DistanceTo(this.Position) < this.LocalCount).ToList();
-        var localAverage = listAverage(closeCrows.Select(x => x.Position));
+		var closeCrows = allCrows.Where(crow => crow.Position.DistanceTo(this.Position) < this.LocalCount).ToList();
+		var localAverage = listAverage(closeCrows.Select(x => x.Position));
 
-        //Head towards mouse location, avoiding com, equal goal
+		//Head towards mouse location, avoiding com, equal goal
 
-        var toMouse = (mouseLocation - this.Position).Normalized() * 5;
+		var toMouse = (mouseLocation - this.Position).Normalized() * 5;
 
-        if (CurrentFramesOfAddedWeight > 0)
-        {
-            CurrentFramesOfAddedWeight--;
-            toMouse = (CrowHiveMind.Instance.FocusPoint - this.Position).Normalized() * 100;
-        }
+		if (CurrentFramesOfAddedWeight > 0)
+		{
+			CurrentFramesOfAddedWeight--;
+			toMouse = (CrowHiveMind.Instance.FocusPoint - this.Position).Normalized() * 100;
+		}
 
-        var toCOM = (centerOfMass - this.Position).Normalized();
-        var toLocalCOM = (this.Position - localAverage).Normalized();
+		var toCOM = (centerOfMass - this.Position).Normalized();
+		var toLocalCOM = (this.Position - localAverage).Normalized();
 
-        var targetDirection = (
-            randomMultiplier
-            + toMouse
-            + toCOM
-            + (toLocalCOM * 1000)
-        );
+		var targetDirection = (
+			randomMultiplier
+			+ toMouse
+			+ toCOM
+			+ (toLocalCOM * 1000)
+		);
 
-        this.lastDirection = ((lastDirection * this.AverageWeighting) + targetDirection).Normalized();
+		this.lastDirection = ((lastDirection * this.AverageWeighting) + targetDirection).Normalized();
 
-        this.Position += lastDirection * ((float)delta * 300);
-        this.Rotation = lastDirection.Angle();
+		this.Position += lastDirection * ((float)delta * 300);
+		this.Rotation = lastDirection.Angle();
 
 		MonsterTimer -= delta;
 		if(MonsterTimer < 0) {
 			MonsterTimer = MonsterBase;
 			Monster();
 		}
-    }
+	}
 
 	private double MonsterBase = 10;
 	private double MonsterTimer = 10;
@@ -132,10 +132,10 @@ public partial class Crow : Area2D
 		Console.WriteLine("Field Monstered");
 	}
 
-    private Vector2 listAverage(IEnumerable<Vector2> items)
-    {
-        return new Vector2(items.Select(x => x.X).Average(), items.Select(x => x.Y).Average());
-    }
+	private Vector2 listAverage(IEnumerable<Vector2> items)
+	{
+		return new Vector2(items.Select(x => x.X).Average(), items.Select(x => x.Y).Average());
+	}
 
 }
 
