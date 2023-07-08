43f08a897e72e1be0179f0dfd1145cb52ff8ab99
Mika Heares <mhear22@gmail.com>
2023-07-09 01:28:46 +1000

    farmers have lights

--- scenes/Enemies/Farmer.cs
+++ scenes/Enemies/Farmer.cs
@@ -1,29 +1,42 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class Farmer : EnemyEntity
 {
-	public Farmer(): base(100) {}
+	public Farmer() : base(100) { }
 
-	
+	private Sprite2D Light;
+	private List<Crow> Crows;
+
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		animatedSprite2D.Play();
+		this.Light = GetNode<Sprite2D>("Sprite2D");
+		this.Crows = GetTree().GetNodesInGroup("crows").Select(x=>x as Crow).ToList();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		var ClosestCrow = this.Crows.OrderBy(x=>x.Position.DistanceTo(this.Position)).FirstOrDefault();
+
+		var directionToClosestCrow = (this.Position - ClosestCrow.Position).Normalized();
+
+		directionToClosestCrow = directionToClosestCrow.Rotated((float)-1.55);
+
+		this.Light.Rotation = directionToClosestCrow.Angle();
 	}
 
-    protected override void onKilled()
-    {
+	protected override void onKilled()
+	{
 		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		animatedSprite2D.Hide();
-    }
+	}
 	public void _on_area_entered(Crow crow)
 	{
 		this.TakeHit();
