2d943988394764760e3086a1c00ad1496b7f9641
Hedd Schotte <subzerofusion@gmail.com>
2023-07-09 21:46:03 +1000

    Panning!

--- scenes/Camera/Camera.cs
+++ scenes/Camera/Camera.cs
@@ -3,32 +3,62 @@ using System;
 
 public partial class Camera : Camera2D
 {
-	[Export]
-	public float ZoomSpeed = 1.1f;
+  [Export]
+  public float ZoomSpeed = 1.1f;
 
-	[Export]
-	public float MaxZoom = 10f;
+  [Export]
+  public float MaxZoom = 10f;
 
-	[Export]
-	public float MinZoom = 0.1f;
+  [Export]
+  public float MinZoom = 0.1f;
 
-	[Export]
-	public float PanThreshold = 200f;
+  [Export]
+  public float PanThreshold = 200f;
 
-	[Export]
-	public float PanSpeed = 1f;
+  [Export]
+  public float PanSpeed = 1f;
 
-	public Vector2 Velocity = Vector2.Zero;
+  public Vector2 Velocity = Vector2.Zero;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		var rec = GetViewport().GetVisibleRect();
-		var v = GetLocalMousePosition() + rec.Size/2;
+  // Called every frame. 'delta' is the elapsed time since the previous frame.
+  public override void _Process(double delta)
+  {
+    var rect = GetViewport().GetVisibleRect();
+    var mousePos = GetViewport().GetMousePosition();
 
-		if(rec.Size.X - v.X <= PanThreshold || v.X <= PanThreshold || rec.Size.Y - v.Y <= PanThreshold || v.Y <= PanThreshold)
-		{
-			this.Position = Position.Lerp(GetGlobalMousePosition(), (float)(PanSpeed * delta));
-		}
-	}
+    var topLeft = rect.Position;
+    var botRight = rect.End;
+
+    Vector2 pan = Vector2.Zero;
+
+    // if we're in the pan threshold
+    pan.X = Mathf.Min((mousePos.X - (botRight.X - PanThreshold)) / PanThreshold, 1);
+    // if we're not in the threshold, try the other side
+    if (pan.X < 0)
+    {
+      pan.X = Mathf.Max((mousePos.X - (topLeft.X + PanThreshold)) / PanThreshold, -1);
+      if (pan.X > 0) pan.X = 0;
+    }
+
+    // if we're in the pan threshold
+    pan.Y = Mathf.Min((mousePos.Y - (botRight.Y - PanThreshold)) / PanThreshold, 1);
+    // if we're not in the threshold, try the other side
+    if (pan.Y < 0)
+    {
+      pan.Y = Mathf.Max((mousePos.Y - (topLeft.Y + PanThreshold)) / PanThreshold, -1);
+      if (pan.Y > 0) pan.Y = 0;
+    }
+
+    // GD.Print(Position + " " + Position + pan * PanSpeed * (float)delta);
+    pan = Position + pan * PanSpeed * (float)delta;
+    Position = pan.Clamp(new Vector2(LimitLeft, LimitTop), new Vector2(LimitRight, LimitBottom));
+  }
+
+  // public override void _Input(InputEvent @event)
+  // {
+  //   if (@event is InputEventMouseButton mouseButton && mouseButton.Pressed && mouseButton.ButtonIndex == MouseButton.Left)
+  //   {
+  //     CurrentFramesOfAddedWeight = this.FramesOfAddedWeight;
+  //   }
+  // }
 }
